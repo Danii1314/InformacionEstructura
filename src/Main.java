6957af798d54df4ff139e5e7b76f8522982bcaff
Danii1314 <d.silva.andres73@hotmail.com>
2024-09-30 11:10:43 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //Daniel Andres Silva Lopez
+        //daniel.silva-l@uniminuto.com
+        //Tenjo-Cundinamarca
+        //Trabajar
+    }
+}
